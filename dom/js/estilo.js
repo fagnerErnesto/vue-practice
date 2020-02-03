7456a39673adc030b52ee5519f749b30c3922f23
Fagner Alves Ernesto <fagner.ernesto@gmail.com>
2020-02-03 13:17:24 -0300

    Estilo dinamico 2

--- dom/js/estilo.js
+++ dom/js/estilo.js
@@ -2,12 +2,23 @@ new Vue({
     el: '#app',
     data: {
         aplicarC1: false,
+        classeCss: 'c1',
+        aplicarGirar: false
     },
     computed: {
         estilo1() {
             return {
                 c1: this.aplicarC1,
                 c2: !this.aplicarC1
+            }
+        }
+    },
+    methods: {
+        girar() {
+            if (this.aplicarGirar) {
+                this.aplicarGirar = false;
+            } else {
+                this.aplicarGirar = true;
             }
         }
     }
